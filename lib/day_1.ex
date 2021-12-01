0b9b14c06e450bdad752128a9ddf30d0db640ba4
Štefan Ľupták <sluptak@me.com>
2021-12-01 10:56:49 +0100

    Add specs

--- lib/day_1.ex
+++ lib/day_1.ex
@@ -1,8 +1,5 @@
 defmodule Day1 do
-  #
-  # part 1
-  #
-
+  @spec part_1([integer()]) :: integer()
   def part_1(input), do: part_1(input, 0)
   def part_1([], increases_count), do: increases_count
   def part_1([_], increases_count), do: increases_count
@@ -12,21 +9,16 @@ defmodule Day1 do
 
   def part_1([_a, b | tail], increases_count), do: part_1([b | tail], increases_count)
 
-  #
-  # part 2
-  #
-
+  @spec part_2([integer()]) :: integer()
   def part_2(input), do: part_2(input, 0)
   def part_2([], increases_count), do: increases_count
   def part_2([_], increases_count), do: increases_count
   def part_2([_, _], increases_count), do: increases_count
   def part_2([_, _, _], increases_count), do: increases_count
 
-  def part_2([a, b, c, d | tail], increases_count) do
-    if b + c + d > a + b + c do
-      part_2([b, c, d | tail], increases_count + 1)
-    else
-      part_2([b, c, d | tail], increases_count)
-    end
-  end
+  def part_2([a, b, c, d | tail], increases_count) when d > a,
+    do: part_2([b, c, d | tail], increases_count + 1)
+
+  def part_2([_a, b, c, d | tail], increases_count),
+    do: part_2([b, c, d | tail], increases_count)
 end

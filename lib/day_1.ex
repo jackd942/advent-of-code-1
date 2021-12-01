33804c922be4a8260c5817f27421998431983acf
Štefan Ľupták <sluptak@me.com>
2021-12-01 10:41:15 +0100

    Day 1

---
+++ lib/day_1.ex
@@ -0,0 +1,32 @@
+defmodule Day1 do
+  #
+  # part 1
+  #
+
+  def part_1(input), do: part_1(input, 0)
+  def part_1([], increases_count), do: increases_count
+  def part_1([_], increases_count), do: increases_count
+
+  def part_1([a, b | rest], increases_count) when b > a,
+    do: part_1([b | rest], increases_count + 1)
+
+  def part_1([_a, b | rest], increases_count), do: part_1([b | rest], increases_count)
+
+  #
+  # part 2
+  #
+
+  def part_2(input), do: part_2(input, 0)
+  def part_2([], increases_count), do: increases_count
+  def part_2([_], increases_count), do: increases_count
+  def part_2([_, _], increases_count), do: increases_count
+  def part_2([_, _, _], increases_count), do: increases_count
+
+  def part_2([a, b, c, d | rest], increases_count) do
+    if b + c + d > a + b + c do
+      part_2([b, c, d | rest], increases_count + 1)
+    else
+      part_2([b, c, d | rest], increases_count)
+    end
+  end
+end
